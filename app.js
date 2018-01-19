d624c2191d64c3cb30e1a26324fa4e7a94368325
Nicole Woodard <NicoleW@westcstdata.com>
2018-01-19 15:14:06 -0700

    Visual modifications and preparation to add to github

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+
+//Middleware
 const express = require('express') //basic express driver
 const path = require('path') //assists with serving static files and dirs
+const helmet = require('helmet') //security middleware
+const sql = require('mssql') //SQL Server interaction
+
+//Aliases
 const app = express() //rename express to app
-const router = express.Router();
+const router = express.Router(); //rename express.Router for easier access
+
+//Page Modules
+const compDataSelMod = require('./modules/compDataSel.js')
 
 
+app.use(helmet()) //instantiate helmet
 app.use(router) //create an instance of router
+
 router.get( '/', (req,res) => {res.render('index') })
+const index = require('index.js')
+
 router.get( '/compDataSel', (req,res) => {res.render('compDataSel') })
+
+
 
 app.set('views',path.join(__dirname, "views")) //tell pug where views folder is
 app.set('view engine', 'pug') //configure html template language
